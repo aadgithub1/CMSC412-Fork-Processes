d504df8489efdd7bf76348c4b24b4c6794881bb5
Aaron <152732836+aadgithub1@users.noreply.github.com>
2025-01-14 09:52:48 -0500

    Fix execution order using wait().

--- forks.c
+++ forks.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(){
     //we have an initial parent proecess P
@@ -14,18 +15,24 @@ int main(){
         printf("fork failed\n");
     } else if(child_1_pid == 0)
     {
+        //this is the first child process block
         printf("i am the first child! parent id: %d\n", getppid());
     } else
     {
+        //currently parent block (before second fork)
+        wait(&status);
         child_2_pid = fork();
         if(child_2_pid == -1)
         {
             printf("second fork failure\n");
         } else if(child_2_pid == 0)
         {
+            //second child process block
             printf("i am the second child! parent pid: %d\n", getppid());
         } else
         {
+            //new parent block (after second fork)
+            wait(&status);
             printf("i am the parent with ID %d\n", getpid());
         }
     }
